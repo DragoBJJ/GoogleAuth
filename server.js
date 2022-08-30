58c6518d3005ad4c113e3d3885d3fc8181ab374a
DragoBJJ <j.pawelski.it@gmail.com>
2022-08-30 18:51:07 +0200

    set cookie session

--- server.js
+++ server.js
@@ -1,28 +1,85 @@
 import fs from "fs";
 import https from "http";
+import dotenv from "dotenv";
 import express from "express";
 import helmet from "helmet";
 import path from "path";
+import passport from "passport";
 import { checkLoggedIn } from "./middleware/loginMiddleware.js";
 import { fileURLToPath } from "url";
+import { Strategy } from "passport-google-oauth20";
+import cookieSession from "cookie-session";
 
+dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
+};
+
+const AUTH_OPTIONS = {
+  callbackURL: "/auth/google/callback",
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+};
+
+const verifyCallback = (accessToken, refreshToken, profile, done) => {
+  console.log("google profile", profile);
+  done(null, profile);
+};
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
 const app = express();
+
 app.use(helmet());
+app.use(
+  cookieSession({
+    name: "session",
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+  })
+);
+app.use(passport.initialize());
 const PORT = 3000;
 
-app.get("/auth/google", (req, res) => {});
-app.get("/auth/google/callback", (req, res) => {});
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["email"],
+  })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate(
+    "google",
+    {
+      failureRedirect: "/failure",
+      successRedirect: "/",
+      session: false,
+    },
+    (req, res) => {
+      console.log("Google called us back !");
+    }
+  )
+);
+
+app.get("/failure", (req, res) => {
+  return res.send("Failed to log in !");
+});
 
 app.get("/auth/logout", (req, res) => {});
 
 app.get("/secret", checkLoggedIn, (req, res) => {
-  res.send("Your personal secret value is 42");
+  res.status(200).send("Your personal secret value is 42");
 });
 
-app.get("/", (res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
